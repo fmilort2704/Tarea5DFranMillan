2bd9b3d96467c26020cdfcbfadfadd9ad1adbeca
fmilort2704 <fmilort2704@g.educaand.es>
2024-01-15 09:33:43 +0100

    Añade atributos en clase metodos

---
+++ src/main/java/com/mycompany/tarea5d_franciscomillanortiz/Catalogos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tarea5d_franciscomillanortiz;
+
+/**
+ *
+ * @author francisco
+ */
+public class Catalogos {
+    private String titulo;
+    private String director;
+    private String categoria;
+    private int notaCritica;
+    private int duracionMinutos;
+    
+}
