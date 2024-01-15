415c6430a1ec284552540898a3df06dd85b559ff
None083 <perezgonzaleznoelia08@gmail.com>
2024-01-15 10:15:15 +0100

    Añade main, objetos c1 y c2, y souts

---
+++ src/main/java/com/mycompany/tarea5d_franciscomillanortiz/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.tarea5d_franciscomillanortiz;
+
+/**
+ *
+ * @author noelia
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Catalogos c1 = new Catalogos("hola", "que", "tal", 
+                1, 2);
+        
+        Catalogos c2 = new Catalogos("nos", "vamos", "ya", 
+                3, 4);
+        
+        System.out.println(c1);
+        System.out.println(c2);
+        
+    }
+    
+}
